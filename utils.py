f6ef006b38b3c1f57f9581746a0af24d28376408
Zbynkus9 <kamil.pojedynek@gmail.com>
2026-05-07 17:53:38 +0200

    feat: Dec to Bin converter

--- utils.py
+++ utils.py
@@ -19,3 +19,14 @@ def multiply(a: int, b: int) -> int:
 def divide(a: int, b: int) -> float:
     """This function divides two numbers."""
     return a / b
+
+
+def int_to_bin(n: int) -> str:
+    """This function converts an integer to binary."""
+    if not isinstance(n, int) or isinstance(n, bool):
+        raise TypeError("Value must be an integer.")
+
+    if n < 0 or n > 100:
+        raise ValueError("Number must be between 0 and 100.")
+
+    return bin(n)[2:]
