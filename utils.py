4fd5c4e13f0715835ad47395a9cff4e9483b9ed6
Zbynkus9 <kamil.pojedynek@gmail.com>
2026-05-07 17:42:10 +0200

    refactor: pylint fix

--- utils.py
+++ utils.py
@@ -1,17 +1,20 @@
-# Exemplary calculator functions
-
+"""This module contains utility functions."""
 
 def add(a: int, b: int) -> int:
+    """This function adds two numbers."""
     return a + b
 
 
 def subtract(a: int, b: int) -> int:
+    """This function subtracts two numbers."""
     return a - b
 
 
 def multiply(a: int, b: int) -> int:
+    """This function multiplies two numbers."""
     return a * b
 
 
 def divide(a: int, b: int) -> float:
+    """This function divides two numbers."""
     return a / b
